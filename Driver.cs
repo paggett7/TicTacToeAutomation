4c7ac707b670f375e14629e12d2b441ff5dff207
paggett7 <paggett7@gmail.com>
2019-04-16 20:29:22 -0400

    committing tic tac toe changes

---
+++ Driver.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendlyAutomation
+{
+    public class Driver
+    {
+        public IWebDriver CalendlyDriver;
+
+        [SetUp]
+        public void StartDriver()
+        {
+            CalendlyDriver = new ChromeDriver(@"C:\ChromeDriver");
+            CalendlyDriver.Url = "https://codepen.io/jshlfts32/full/bjambP/";
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            CalendlyDriver.Close();
+        }
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalendlyAutomation
+{
+    public class TestClass : Driver
+    {
+
+        [Test]
+        public void CheckPageTitle()
+        {
+            Thread.Sleep(3000);
+            var pageTitle = CalendlyDriver.FindElement(By.Id("editable-title-span")).Text;
+            Assert.IsTrue(pageTitle == "Broken (on purpose) Tic Tac Toe");
+        }
+
+        [Test]
+        public void TicTacToeGridDisplayed()
+        {
+            CalendlyDriver.SwitchTo().Frame(CalendlyDriver.FindElement(By.Id("result")));
+            CalendlyDriver.FindElement(By.Id("number")).SendKeys("3");
+            CalendlyDriver.FindElement(By.XPath(@"//button[@id='start']")).Click();
+            bool tableDisplayed = CalendlyDriver.FindElement(By.Id("table")).Displayed;
+            Assert.IsTrue(tableDisplayed);
+
+            int countSqures = CalendlyDriver.FindElements(By.XPath("//td")).Count-1;
+            int i = 0;
+            while (i <= countSqures)
+            {
+                bool endGame = CalendlyDriver.FindElement(By.Id("endgame")).Displayed;
+                if (!endGame)
+                {
+                    CalendlyDriver.FindElement(By.Id("" + i + "")).Click();
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Thread.Sleep(5000);
+        }
+    }
+}
